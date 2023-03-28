085d8814d6936ebace2b28a66b5c7267292b4d49
Marcos Calderon <mcalderonig@gmail.com>
2023-03-27 19:08:16 -0600

    cart context

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import React, {useState} from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import Cart from "./Components/Cart/Cart";
+//Providers
+import CartProvider from "./store/CartProvider";
 function App() {
   //  Hooks
   const [cartIsShown, setCartIsShown] = useState(false);
   const manageCartHandler = () => {
     cartIsShown ? setCartIsShown(false) : setCartIsShown(true);
   };
-  
+
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={manageCartHandler}/>}
       <Header onShowCart={manageCartHandler}/>
       <main>
         <Meals/>
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
